fed1055a28dd7e2affd61ee4b2718fdbf8781dd8
ldrd <fealnor@gmail.com>
2019-07-21 20:03:21 +0200

    Gère les packets génériques

---
+++ Server/Network/Bytebuffer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Network
+{
+    class Bytebuffer
+    {
+        private int rpos = 0;
+        protected List<byte> data;
+        public Bytebuffer(byte[] data)
+        {
+            this.data = new List<byte>(data);
+        }
+
+        public Bytebuffer()
+        {
+            data = new List<byte>();
+        }
+
+        public T Read<T>()
+        {
+            var type = typeof(T);
+
+            if (type == typeof(Byte))
+                return (T)(object)data[rpos++];
+            else if (type == typeof(Int16))
+                return (T)(object)(data[rpos++] | data[rpos++] << 8);
+            else if (type == typeof(Int32))
+                return (T)(object)(data[rpos++] | data[rpos++] << 8 | data[rpos++] << 16 | data[rpos++] << 24);
+
+            throw new NotImplementedException();
+        }
+
+        public void Append<T>(T value)
+        {
+            var type = typeof(T);
+
+            if (type == typeof(Byte))
+                data.Add((byte)(object)value);
+            else if (type == typeof(Int16))
+                data.AddRange(EnsureLittleEndian(BitConverter.GetBytes((Int16)(object)value)));
+            else if (type == typeof(Int32))
+                data.AddRange(EnsureLittleEndian(BitConverter.GetBytes((Int32)(object)value)));
+        }
+
+        private byte[] EnsureLittleEndian(byte[] bytes)
+        {
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return bytes;
+        }
+
+        public void SkipBytes(int count)
+        {
+            rpos += count;
+        }
+
+        public Int16 GetSize()
+        {
+            return (Int16)data.Count;
+        }
+
+        public byte[] GetData()
+        {
+            return data.ToArray();
+        }
+    }
+}
---
+++ Server/Network/Packet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Network
+{
+    abstract class Packet : Bytebuffer
+    {
+        public Packet(Packet packet)
+        {
+            data = new List<byte>(packet.GetData());
+            Read();
+        }
+
+        public Packet() { }
+
+        public abstract void Read();
+    }
+}
