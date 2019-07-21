62a677b827c7a4e22f64b5cbb9d73197c5b52894
ldrd <fealnor@gmail.com>
2019-07-21 22:47:13 +0200

    Ajoute le packet pour le gameplay

---
+++ Server/Network/Packets/GameplayPacket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Network.Packets
+{
+    internal class GameplayPacket : Packet
+    {
+        public Int32 infos;
+
+        public GameplayPacket(WorldPacket worldPacket) : base(worldPacket) { }
+
+        public GameplayPacket(Int32 infos) : base()
+        {
+            this.infos = infos;
+            worldPacket.SetOpCode(OpcodeMsg.GameplayPacket);
+            worldPacket.Append(this.infos);
+        }
+
+        protected override void Read()
+        {
+            infos = worldPacket.Read<Int32>();
+        }
+    }
+}
