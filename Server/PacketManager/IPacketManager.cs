3439dae9c280fd2b48ad1a3c2714a691e703f4df
ldrd <fealnor@gmail.com>
2019-07-24 20:14:11 +0200

    Ajoute le dispatcher de worldpacket

---
+++ Server/PacketManager/IPacketManager.cs
@@ -0,0 +1,14 @@
+﻿using Server.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.PacketManager
+{
+    public interface IPacketManager
+    {
+        void Manage(WorldPacket worldpacket);
+    }
+}
---
+++ Server/WorldPacketDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Network;
+using Server.PacketManager;
+
+namespace Server
+{
+    //Lie un opcode à un manager
+    public class WorldPacketDispatcher
+    {
+        private readonly Dictionary<OpcodeMsg, IPacketManager> link = new Dictionary<OpcodeMsg, IPacketManager>();
+
+        public WorldPacketDispatcher()
+        {
+            
+        }
+
+        public void Dispatch(WorldPacket worldPacket)
+        {
+            link[worldPacket.GetOpcode()].Manage(worldPacket);
+        }
+    }
+}
